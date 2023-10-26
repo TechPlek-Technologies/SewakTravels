5e4790d92f2b12960648662691916e0733ba6892
Deepaksharma-39 <d33paksharma.39@gmail.com>
2023-10-26 10:30:32 +0530

    uri completed

--- src/Components/Common/DatePickerComponent.jsx
+++ src/Components/Common/DatePickerComponent.jsx
@@ -3,7 +3,6 @@ import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
 
 function DatePickerComponent({ setStart, start }){
-    console.log(start)
 
 return(
 <DatePicker  value={start} onChange={(date) => setStart(date)} id="datepicker" className="datepicker-main" dateFormat="dd MMMM"  />  
