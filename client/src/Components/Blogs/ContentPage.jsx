f27d501b6d0a72fec60d77341af2590f3325124b
Deepaksharma-39 <d33paksharma.39@gmail.com>
2024-01-19 14:47:46 +0530

    blog modify

--- client/src/Components/Blogs/ContentPage.jsx
+++ client/src/Components/Blogs/ContentPage.jsx
@@ -1,21 +1,28 @@
 import BodyContent from "./BodyContent";
 
 const ContentPage = ({ slideData, view }) => {
-    
   // console.log(slideData)
-    return (
-      <>
-       <div className={`row ${view === "creative" || view === "list" ? "blog-list" : ""}`}>
-          {slideData?.map((items,index) => (
-            <div className={"col-12"} key={index}>
-              <BodyContent data={items} view={view} />
-            </div>
-          ))}
-        </div>
-       
-  
-      </>
-    );
-  };
-  
-  export default ContentPage;
+  return (
+    <>
+      <div
+        className={`row ${
+          view === "creative" || view === "list" ? "blog-list" : ""
+        }`}
+      >
+        {slideData?.map((items, index) => {
+          if (index === 0) {
+            return "";
+          } else {
+            return (
+              <div className={"col-12"} key={index}>
+                <BodyContent data={items} view={view} />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ContentPage;
