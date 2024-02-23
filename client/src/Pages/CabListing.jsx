ed69603e7e871d3473c72e57ad0ef80782a6a9e2
deepaksharma-39 <d33paksharma.39@gmail.com>
2024-02-23 12:57:52 +0530

    dynamic rendering

--- client/src/Pages/CabListing.jsx
+++ client/src/Pages/CabListing.jsx
@@ -3,6 +3,7 @@ import Layout from "../Layout/Layout"
 import ListSearch from "../Components/Cab/Listing/ListSearch";
 import FooterComponent from "../Components/Common/FooterComponent";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function CabListing(){
@@ -13,11 +14,21 @@ function CabListing(){
 
   const [isValid,setisValid]=useState("notValid");
  
+
+  const {source,destination}=useParams();
+
+  console.log(source,destination);
     return(
         <>
         <Layout title="light_header"/>
         <ListSearch setisValid={setisValid}/>
         <ListingView isValid={isValid}/>
+
+        {source.toUpperCase()=="DELHI, INDIA" &&  destination.toUpperCase()==="CHANDIGARH, INDIA" && <div>CHANDIGARH</div> }
+        {source.toUpperCase()=="DELHI, INDIA" &&  destination.toUpperCase()==="AGRA, UTTAR PRADESH, INDIA" && <div>AGRA</div> }
+        {source.toUpperCase()=="DELHI, INDIA" &&  destination.toUpperCase()==="DEHRADUN, UTTARAKHAND, INDIA" && <div>DEHRADUN</div> }
+        {source.toUpperCase()=="DELHI, INDIA" &&  destination.toUpperCase()==="JAIPUR, RAJASTHAN, INDIA" && <div>JAIPUR</div> }
+        
         <FooterComponent/>
       </>
     )
