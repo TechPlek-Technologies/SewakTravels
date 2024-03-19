18291f333fcf4c3f6f26eb223b3aa8a7d4d4031a
deepaksharma-39 <d33paksharma.39@gmail.com>
2024-03-19 22:32:07 +0530

    add post request orders

--- client/src/Utility/ca_admin.jsx
+++ client/src/Utility/ca_admin.jsx
@@ -9,3 +9,24 @@ export const fetchData = async () => {
       throw err; // Rethrow the error so it can be caught in the component
     }
   };
+
+  export const fetchBillingData = async () => {
+    try {
+      const result = (await axios.get(`https://new.sewaktravels.com/billing-details`)).data;
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw err; // Rethrow the error so it can be caught in the component
+    }
+  };
+
+
+export const addBillingData = async (payments) => {
+  try {
+    const response = await axios.post('https://new.sewaktravels.com/payments', payments);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw err; // Rethrow the error so it can be caught in the component
+  }
+};
